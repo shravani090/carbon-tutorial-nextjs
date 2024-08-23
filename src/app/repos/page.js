869396ccbca88949503306b3e74b79e8ba8364a0
Shravani B <Shravani.B@ibm.com>
2024-08-23 15:40:35 +0530

    feat(tutorial): complete step 2

--- src/app/repos/page.js
+++ src/app/repos/page.js
@@ -1,7 +1,12 @@
-`use client`
-
+'use client';
+import { Grid, Column } from '@carbon/react';
+import RepoTable from './RepoTable';
 export default function RepoPage() {
-    return (
-        <div>REPO PAGE</div>
-    );
-};
+  return (
+    <Grid className="repo-page">
+      <Column lg={16} md={8} sm={4} className="repo-page__r1">
+        <RepoTable headers={headers} rows={rows} />
+      </Column>
+    </Grid>
+  );
+}
